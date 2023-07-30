b198c7b2d5915186adf2dc5c2eb834c6ddae69d0
Park Sangeun <90459890+park-sangeun@users.noreply.github.com>
2023-07-30 21:26:14 +0900

    Add files via upload

---
+++ 1. Login/idLogin/idLogin/src/main/java/com/idLogin/idLogin/entity/SDataEntity.java
@@ -0,0 +1,49 @@
+package com.idLogin.idLogin.entity;
+
+import com.idLogin.idLogin.dto.SDataDTO;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "sRobot_info")
+public class SDataEntity {
+    @Id // pk 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Long sRobot_info_id;
+
+    @Column
+    @NotNull
+    private Float sHumidity;
+
+    @Column
+    @NotNull
+    private Float sTemp;
+
+    @Column
+    @Builder.Default
+    private Double sLocate = 0.0;
+
+    public static SDataEntity toSDataEntity(SDataDTO sdataDTO) {
+        SDataEntity sdataEntity = new SDataEntity();
+        sdataEntity.setSHumidity(sdataDTO.getSHumidity());
+        sdataEntity.setSTemp(sdataDTO.getSTemp());
+        sdataEntity.setSLocate(sdataDTO.getSLocate());
+        return sdataEntity;
+    }
+
+    public static SDataEntity toUpdateSDataEntity(SDataDTO sdataDTO) {
+        SDataEntity sdataEntity = new SDataEntity();
+        sdataEntity.setSRobot_info_id(sdataDTO.getSRobot_info_id());
+        sdataEntity.setSHumidity(sdataDTO.getSHumidity());
+        sdataEntity.setSTemp(sdataDTO.getSTemp());
+        sdataEntity.setSLocate(sdataDTO.getSLocate());
+        return sdataEntity;
+    }
+}
