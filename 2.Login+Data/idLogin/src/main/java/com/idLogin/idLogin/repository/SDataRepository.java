725c51945c1f446438e4ea72bf62e0ae4bb46c8c
Park Sangeun <90459890+park-sangeun@users.noreply.github.com>
2023-08-11 15:30:03 +0900

    Add files via upload

---
+++ 2.Login+Data/idLogin/src/main/java/com/idLogin/idLogin/repository/SDataRepository.java
@@ -0,0 +1,11 @@
+package com.idLogin.idLogin.repository;
+
+import com.idLogin.idLogin.entity.SDataEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SDataRepository {
+}
