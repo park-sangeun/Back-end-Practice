4c82bc656b08437a9617796ebea67432e2c9120c
Park Sangeun <90459890+park-sangeun@users.noreply.github.com>
2023-08-11 15:31:07 +0900

    Delete 2.Login+Data/idLogin directory

--- 2.Login+Data/idLogin/src/main/java/com/idLogin/idLogin/repository/VideoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.idLogin.idLogin.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.idLogin.idLogin.entity.Files;
-
-public interface VideoRepository extends JpaRepository<Files, Integer> {
-    Files findByFno(int fno);
-}
