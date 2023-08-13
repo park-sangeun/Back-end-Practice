3847cc027d4f4a2d8cefd6dfd232eb407d46c152
Park Sangeun <90459890+park-sangeun@users.noreply.github.com>
2023-08-13 22:49:01 +0900

    Add files via upload

---
+++ 2. Data/idLogin/src/main/java/com/idLogin/idLogin/dto/FilesDTO.java
@@ -0,0 +1,20 @@
+package com.idLogin.idLogin.dto;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FilesDTO {
+    private Long fno;
+    private String filename;
+    private Date uploadDate;
+
+    // 매개변수를 받아 초기화하는 생성자
+
+    // 생성자, getter, setter 등 필요한 메서드를 추가할 수 있음
+
+}
